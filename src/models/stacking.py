3b87179da4df6423f3c2f7392b11bde78fe67086
androiderNN <kento.i.1103@gmail.com>
2024-10-27 20:22:03 +0900

    stacking時の事後処理を実装 lstmのstackingではpuclic score変化せず

--- src/models/stacking.py
+++ src/models/stacking.py
@@ -14,12 +14,49 @@ import model_base, model_torch_base, model_cnn
 now = datetime.datetime.now()
 time = now.strftime('%m%d_%H:%M:%S')
 
+def getmean(pred):
+    pred = np.array(pred)
+
+    pred = pred.reshape((pred.shape[0], int(pred.shape[1]/30), 30, pred.shape[2]))  # (num_preds, 1200, 30, 9ぐらい)
+    pmean = pred.mean(axis=0)   # (1200, 30, 9ぐらい)
+
+    # z軸方向の速度が0のデータについてxの変動が大きいデータの削除
+    zmax = pmean[:,:,-1].max(axis=1)
+    zmin = pmean[:,:,-1].min(axis=1)
+    zdiff = zmax - zmin
+    zmask = (zdiff<0.5) & (zmax<0.5) & (zmin>-0.5)   # z方向の速度がほぼ0のtrialのmask
+
+    # 各trialでxの速度が条件を満たす予測値のみを用いて平均値を取り直す
+    for i in range(pmean.shape[0]):
+        if zmask[i]:    # zの速度が0周辺
+            x = pred[:,i,:,-3]
+            
+            xdiff = x.max(axis=1) - x.min(axis=1)
+            xmask1 = xdiff < 1.5   # xのdiffが1.5未満
+
+            # zが0付近の時は平面上の滑走なので速度xの絶対値は一般に大きい
+            xmask2 = abs(x).min(axis=1) > 2
+
+            xmask = xmask1 & xmask2 # maskの統合
+            
+            if sum(xmask)>0:    # 条件を満たすxの予測値が存在する場合
+                tmp = pred[xmask,i,:,-3]
+                pmean[i,:,-3] = tmp.mean(axis=0)
+    
+    pmean = pmean.reshape(-1,pmean.shape[2])
+    
+    return pmean
+
 def meanstack():
     stack_ids = [
-        'lgb_1020_12:55:50',
-        'lgb_1020_12:57:17',
-        'lgb_1020_12:58:38',
-        'lgb_1020_13:00:14'
+        # 'lgb_1027_01:55:57',
+        # 'lgb_1027_01:57:32',
+        # 'lgb_1027_01:59:33',
+        # 'lgb_1027_02:01:09',
+        'lstm_1027_07:24:15',
+        'lstm_1027_07:33:41',
+        'lstm_1027_07:43:26',
+        'lstm_1027_08:00:30',
     ]
     train_pred = [pickle.load(open(os.path.join(config.exdir, id, 'train_pred.pkl'), 'rb')) for id in stack_ids]
     test_pred = [pickle.load(open(os.path.join(config.exdir, id, 'test_pred.pkl'), 'rb')) for id in stack_ids]
@@ -27,10 +64,11 @@ def meanstack():
     tr_col = train_pred[0].columns
     te_col = test_pred[0].columns
 
-    
-    train_pred = np.array(train_pred).mean(axis=0)
+    # 平均値の算出
+    train_pred = getmean(train_pred)
+    test_pred = getmean(test_pred)
+
     train_pred = pd.DataFrame(train_pred, columns=tr_col)
-    test_pred = np.array(test_pred).mean(axis=0)
     test_pred = pd.DataFrame(test_pred, columns=te_col)
 
     tr_rmse = model_base.rmse_3d(train_pred)
@@ -43,6 +81,8 @@ def meanstack():
     os.mkdir(dirpath)
     test_pred = test_pred.astype({'sid': int, 'trial': int, 'timepoint': int})
     model_base.make_submission(test_pred, dirpath)
+    pickle.dump(train_pred, open(os.path.join(dirpath, 'train_pred.pkl'), 'wb'))
+    pickle.dump(test_pred, open(os.path.join(dirpath, 'test_pred.pkl'), 'wb'))
 
     print('finished')
 
@@ -64,13 +104,22 @@ class model_stack_cnn(nn.Module):
         # x = self.linear(x)
         return x
 
-class stack():
+class stack_cnn():
     def __init__(self, params):
         self.stack_models = {
-            'lgb': 'lgb_1015_07:11:19_cv',
-            'cnn': 'cnn_1015_07:13:48_cv',
-            'lstm': 'lstm_1015_07:55:58_cv',
+            # 'lgb': 'lgb_1015_07:11:19_cv',
+            # 'cnn': 'cnn_1015_07:13:48_cv',
+            # 'lstm': 'lstm_1015_07:55:58_cv',
             # 'cnn_lstm': 'cnn-lstm_1013_19:26:48',
+
+            # 'lgb1': 'lgb_1027_01:55:57',
+            # 'lgb2': 'lgb_1027_01:57:32',
+            # 'lgb3': 'lgb_1027_01:59:33',
+            # 'lgb4': 'lgb_1027_02:01:09',
+            'lstm1': 'lstm_1027_07:24:15',
+            'lstm2': 'lstm_1027_07:33:41',
+            'lstm3': 'lstm_1027_07:43:26',
+            'lstm4': 'lstm_1027_08:00:30',
         }
 
         self.params = params
@@ -85,7 +134,8 @@ class stack():
     def dataset(self):
         '''
         {target: [(tr_trials,30,n_models)*2, (te_trials,30,n_models)]}'''
-        train_preds = {k: pickle.load(open(os.path.join(config.exdir, self.stack_models[k], 'train_cv_pred.pkl'), 'rb')) for k in self.stack_models.keys()}
+        # train_preds = {k: pickle.load(open(os.path.join(config.exdir, self.stack_models[k], 'train_cv_pred.pkl'), 'rb')) for k in self.stack_models.keys()}
+        train_preds = {k: pickle.load(open(os.path.join(config.exdir, self.stack_models[k], 'train_pred.pkl'), 'rb')) for k in self.stack_models.keys()}
         test_preds = {k: pickle.load(open(os.path.join(config.exdir, self.stack_models[k], 'test_pred.pkl'), 'rb')) for k in self.stack_models.keys()}
 
         self.data = dict()
